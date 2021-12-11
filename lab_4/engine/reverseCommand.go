b2ccb2d44c32afc49d85a6fe354a2ad80fb120ee
liza <gorbunova.elizaveta@gmail.com>
2021-12-11 10:37:37 +0200

    update parser

---
+++ lab_4/engine/reverseCommand.go
@@ -0,0 +1,12 @@
+package engine
+
+import ()
+
+type ReverseCommand struct { 
+  Arg string 
+} 
+
+func (p *ReverseCommand) Execute(loop Handler) { 
+  // reverse it
+	// use printCommand not fmt.println
+} 
