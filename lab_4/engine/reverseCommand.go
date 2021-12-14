cdc3e372b47bb711749eb3bc2fbba95fc2eb1dbd
fhrr-sht <54956154+fhrr-sht@users.noreply.github.com>
2021-12-14 19:52:10 +0200

    Update reverseCommand.go

--- lab_4/engine/reverseCommand.go
+++ lab_4/engine/reverseCommand.go
@@ -1,12 +1,15 @@
 package engine
 
-import ()
+type ReverseCommand struct {
+	Arg string
+}
 
-type ReverseCommand struct { 
-  Arg string 
-} 
-
-func (p *ReverseCommand) Execute(loop Handler) { 
-  // reverse it
-	// use printCommand not fmt.println
-} 
+func (p *ReverseCommand) Execute(loop Handler) {
+	runes := []rune(p.Arg)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	var print PrintCommand
+	print.Arg = string(runes)
+	print.Execute(loop)
+}
