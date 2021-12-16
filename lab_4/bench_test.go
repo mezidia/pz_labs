bad6d9d5294dcefd7667d4e1a32d5b296813b61a
La7rodectus <maxmega27@gmail.com>
2021-12-16 13:00:19 +0200

    add parser bench

--- lab_4/bench_test.go
+++ lab_4/bench_test.go
@@ -4,57 +4,23 @@ import (
 	"fmt"
 	"github.com/mezidia/pz_labs/tree/lab4/lab_4/engine"
 	"github.com/mezidia/pz_labs/tree/lab4/lab_4/parser"
-	"math/rand"
 	"os"
 	"testing"
 )
 
-var temp = os.Stdout
 var cmds []engine.Command
 
 func BenchmarkEventLoop(b *testing.B) {
-
-	f, err := os.Create("test.txt")
-	if err != nil {
-		fmt.Println("error")
-		return
-	}
-	b.SetParallelism(1)
-	for k := 0; k < 10; k++ {
-		l := 2*k + 2
-		_, err := f.WriteString("print тутВсеОкВиводьМене\nreverse FaineMisto\n")
-		if err != nil {
-			fmt.Println("error")
-			return
+	l := 0
+	for k := 0; k < 20; k++ {
+		l += 1000
+		f, _ := os.Create("test.txt")
+		for j := 0; j < l; j++ {
+			f.WriteString("print тутВсеОкВиводьМене\nreverse FaineMisto\n")
 		}
+		b.ResetTimer()
 		b.Run(fmt.Sprintf("len=%d", l), func(b *testing.B) {
 			cmds = parser.Parse("test.txt")
-		})
-	}
-}
-
-func Count(s []int, a int) int {
-	r := 0
-	for _, e := range s {
-		if a == e {
-			r++
-		}
-	}
-	return r
-}
-
-var cntRes int
-
-func BenchmarkCount(b *testing.B) {
-	const baseLen = 5
-	for i := 0; i < 20; i++ {
-		l := baseLen * 1 << i
-		in := make([]int, l)
-		for k := 0; k < l; k++ {
-			in[k] = rand.Intn(10)
-		}
-		b.Run(fmt.Sprintf("len=%d", l), func(b *testing.B) {
-			cntRes = Count(make([]int, l), 0)
 		})
 	}
 }
